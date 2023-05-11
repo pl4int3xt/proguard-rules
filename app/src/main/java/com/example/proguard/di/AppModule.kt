57cc261c4e1b7a55233e9ee1c455059b7c37a6d1
benjahjp <benjahjp@gmail.com>
2023-05-11 15:24:06 +0300

    feat

--- app/src/main/java/com/example/proguard/di/AppModule.kt
+++ app/src/main/java/com/example/proguard/di/AppModule.kt
@@ -1,4 +1,26 @@
 package com.example.proguard.di
 
+import com.example.proguard.common.Constants
+import com.example.proguard.data.api.Api
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideApi(): Api{
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(Api::class.java)
+    }
 }
